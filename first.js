efe75a124045479911f76baa4dced4c23605b69f
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2026-09-01 21:17:14 +0530

    add

--- first.js
+++ first.js
@@ -1,20 +1,15 @@
- function checkPrimeNumber(num) {
-  if (num <= 1) return "this is not a prime number";
+ function isPrime(n) {
+  if (n < 2) return false;
 
-  for (let i = 2; i * i <= num; i++) {
+  if (n === 2 || n === 3) return true;
 
-    console.log( "iteration number : ", i);
+  if (n % 2 === 0 || n % 3 === 0) return false;
 
-    console.log( "square : ",i * i);
-    
-    
-    if (num % i == 0) {
-      return "this is not a prime number";
-    }
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
   }
 
-  return "this is the prime number";
+  return true;
 }
 
-console.log(checkPrimeNumber(457)); // "this is the prime number"
- 
+console.log(isPrime(6));
