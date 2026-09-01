9f2744981f5b8b448c3dd4b7cdec6f24eec1925d
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2026-09-01 22:11:24 +0530

    add

--- first.js
+++ first.js
@@ -1,15 +1,14 @@
- function isPrime(n) {
-  if (n < 2) return false;
+function printFactors(num) {
+  let factors = [];
 
-  if (n === 2 || n === 3) return true;
-
-  if (n % 2 === 0 || n % 3 === 0) return false;
-
-  for (let i = 5; i * i <= n; i += 6) {
-    if (n % i === 0 || n % (i + 2) === 0) return false;
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      factors.push(i);
+    }
   }
-
-  return true;
+  return factors
 }
 
-console.log(isPrime(6));
+const results = printFactors(40)
+
+console.log(results);
